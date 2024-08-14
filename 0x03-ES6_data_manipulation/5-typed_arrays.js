aef570dab215abc25bb43172a78e739415183ab3
Karl <tekamkarl@gmail.com>
2024-08-14 03:34:47 +0100

    Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,15 @@
-export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw new Error('Position outside range');
+function createInt8TypedArray(length, position, value) {
+    const buffer = new ArrayBuffer(length);
+    const view = new DataView(buffer);
+    
+    if (position < 0 || position >= length) {
+      throw new Error('Position outside range');
+    }
+    
+    view.setInt8(position, value);
+    
+    return buffer;
   }
-
-  const view = new Int8Array(length).fill(0);
-  view[position] = value;
-
-  return view.buffer;
-}
-
+  
+  export default createInt8TypedArray;
+  
