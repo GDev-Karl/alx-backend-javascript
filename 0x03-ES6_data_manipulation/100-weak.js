62ebd27e514d61570ce1a68609e56842e4ba7f0a
Karl <tekamkarl@gmail.com>
2024-08-14 03:33:05 +0100

    Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,15 +1,19 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
-  }
-  
-  const count = weakMap.get(endpoint);
-  
-  weakMap.set(endpoint, count + 1);
-  
-  if (count + 1 >= 5) {
-    throw new Error('Endpoint load is high');
+  weakMap.set(endpoint, (weakMap.get(endpoint) || 0) + 1);
+
+  if ((weakMap.get(endpoint) || 0) >= 5) {
+    if (!weakMap.has(endpoint)) {
+      weakMap.set(endpoint, 0);
+    }
+
+    const count = weakMap.get(endpoint);
+
+    weakMap.set(endpoint, count + 1);
+
+    if (count + 1 >= 5) {
+      throw new Error("Endpoint load is high");
+    }
   }
 }
