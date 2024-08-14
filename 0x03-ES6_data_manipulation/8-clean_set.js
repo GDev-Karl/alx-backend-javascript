919f6591d6da1dc84f06e15be86b2f893c3f8118
Karl <tekamkarl@gmail.com>
2024-08-14 03:20:02 +0100

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
-function cleanSet(set, startString) {
-  return Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
-}
+const cleanSet = (set, startString) => {
+  if (startString === '' || typeof startString !== 'string') return '';
+  const strings = Array.from(set).filter(
+    (s) => typeof s === 'string' && s.startsWith(startString),
+  );
+  return strings.map((s) => s.slice(startString.length)).join('-');
+};
 
 export default cleanSet;
