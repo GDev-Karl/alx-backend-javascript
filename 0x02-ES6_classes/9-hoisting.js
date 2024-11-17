a9db41267fea44f19c5dbf3dca0b408512a39769
Karl <tekamkarl@gmail.com>
2024-11-17 19:55:07 +0100

    Fix a code and make it work.

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,21 +1,26 @@
+// Define and export the HolbertonClass class
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
   }
 
+  // Getter for year
   get year() {
     return this._year;
   }
 
+  // Getter for location
   get location() {
     return this._location;
   }
 }
 
+// Instantiate two HolbertonClass objects
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
+// Define and export the StudentHolberton class
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -23,23 +28,28 @@ export class StudentHolberton {
     this._holbertonClass = holbertonClass;
   }
 
+  // Getter for fullName
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
 
+  // Getter for holbertonClass
   get holbertonClass() {
     return this._holbertonClass;
   }
 
+  // Getter for fullStudentDescription
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
+// Create StudentHolberton objects
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
+// Define and export the list of students
 export const listOfStudents = [student1, student2, student3, student4, student5];
